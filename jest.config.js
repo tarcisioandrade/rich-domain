d3b08d97a4ecb650825e0b99d307203a5449ee96
tarcisioandrade <tarcisioandrade016@gmail.com>
2025-11-18 22:56:16 -0300

    chore: enhance Jest configuration and add coverage script to package.json

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,21 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  roots: ['<rootDir>/tests', '<rootDir>/src'],
-  testMatch: ['**/*.test.ts', '**/*.spec.ts'],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/tests", "<rootDir>/src"],
+  testMatch: ["**/*.test.ts", "**/*.spec.ts"],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    "^.+\\.tsx?$": "ts-jest",
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts', '!src/index.ts'],
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov', 'clover'],
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "/dist/",
+    "/tests/",
+    "**/examples/**",
+  ],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  collectCoverageFrom: ["src/**/*.ts", "!src/**/*.d.ts", "!src/index.ts"],
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov", "clover"],
   verbose: true,
 };
