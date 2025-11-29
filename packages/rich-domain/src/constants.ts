fe3bc407ecc094f828ef1da1a8a4b9cf1f1dd7cd
tarcisioandrade <tarcisioandrade016@gmail.com>
2025-11-29 18:30:08 -0300

    feat: add various operator constants for array, boolean, date, number, string, and filter types

--- packages/rich-domain/src/constants.ts
+++ packages/rich-domain/src/constants.ts
@@ -1,7 +1,81 @@
-import { ValidationConfig } from ".";
+import {
+  ArrayOperators,
+  BooleanOperators,
+  DateOperators,
+  FilterOperator,
+  NumberOperators,
+  StringOperators,
+  ValidationConfig,
+} from ".";
 
 export const DEFAULT_VALIDATION_CONFIG: Required<ValidationConfig> = {
   onCreate: true,
   onUpdate: true,
   throwOnError: true,
 };
+
+export const ARRAY_OPERATORS: ArrayOperators[] = [
+  "in",
+  "notIn",
+  "isNull",
+  "isNotNull",
+];
+export const BOOLEAN_OPERATORS: BooleanOperators[] = [
+  "equals",
+  "notEquals",
+  "isNull",
+  "isNotNull",
+];
+export const DATE_OPERATORS: DateOperators[] = [
+  "equals",
+  "notEquals",
+  "greaterThan",
+  "greaterThanOrEqual",
+  "lessThan",
+  "lessThanOrEqual",
+  "in",
+  "notIn",
+  "between",
+  "isNull",
+  "isNotNull",
+];
+export const NUMBER_OPERATORS: NumberOperators[] = [
+  "equals",
+  "notEquals",
+  "greaterThan",
+  "greaterThanOrEqual",
+  "lessThan",
+  "lessThanOrEqual",
+  "in",
+  "notIn",
+  "between",
+  "isNull",
+  "isNotNull",
+];
+export const STRING_OPERATORS: StringOperators[] = [
+  "equals",
+  "notEquals",
+  "contains",
+  "startsWith",
+  "endsWith",
+  "in",
+  "notIn",
+  "isNull",
+  "isNotNull",
+];
+export const FILTER_OPERATORS: FilterOperator[] = [
+  "equals",
+  "notEquals",
+  "greaterThan",
+  "greaterThanOrEqual",
+  "lessThan",
+  "lessThanOrEqual",
+  "contains",
+  "startsWith",
+  "endsWith",
+  "in",
+  "notIn",
+  "between",
+  "isNull",
+  "isNotNull",
+];
