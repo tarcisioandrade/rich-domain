dbacaea8afc13471d0f00716f8f1b733d655bd5f
tarcisioandrade <tarcisioandrade016@gmail.com>
2026-01-03 12:39:07 -0300

    refactor: update getUsers function to return PaginatedJsonResult

--- packages/react-rich-domain/registry/default/service/get-users.ts
+++ packages/react-rich-domain/registry/default/service/get-users.ts
@@ -1,27 +1,29 @@
-import { Criteria, PaginatedResult } from "@woltz/rich-domain";
+import {
+  Criteria,
+  PaginatedResult,
+  type PaginatedJsonResult,
+} from "@woltz/rich-domain";
 import users from "./users.json";
 
 export type TestUser = {
   id: string;
   name: string;
   age: number;
-  user: {
-    lead: {
-      title: string;
-    };
-  }[];
   status: "active" | "inactive";
+  createdAt: string;
 };
 
 export async function getUsers(
   criteria: Criteria = Criteria.create<TestUser>()
-): Promise<PaginatedResult<TestUser>> {
+): Promise<PaginatedJsonResult<TestUser>> {
   try {
     const response = Promise.resolve(users);
     const data = (await response) as TestUser[];
     const result = PaginatedResult.fromArray<TestUser>(data, criteria);
 
-    return result;
+    console.log("criteria", criteria.toJSON());
+    console.log("result", result.toJSON());
+    return result.toJSON();
   } catch (error) {
     console.error(error);
     throw error;
