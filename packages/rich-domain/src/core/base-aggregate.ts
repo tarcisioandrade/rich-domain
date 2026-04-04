11566ab71c578c5ed7edca6345914eb42faee8e8
tarcisioandrade <tarcisioandrade016@gmail.com>
2026-04-04 12:22:12 -0300

    fix: implement WeakMap for domain event management in BaseAggregate

--- packages/rich-domain/src/core/base-aggregate.ts
+++ packages/rich-domain/src/core/base-aggregate.ts
@@ -2,6 +2,19 @@ import { BaseEntity } from "./base-entity.js";
 import { BaseProps, IDomainEvent, IDomainEventBus } from "../types/index.js";
 
 /**
+ * WeakMap-based storage for domain events.
+ * Using a WeakMap avoids ES2022 class field initialization order issues:
+ * class fields are set AFTER super() returns, which would overwrite any
+ * events added during the `onCreate` hook (called inside super()).
+ */
+const eventsStore = new WeakMap<BaseAggregate<any>, IDomainEvent[]>();
+
+function getEvents(instance: BaseAggregate<any>): IDomainEvent[] {
+  if (!eventsStore.has(instance)) eventsStore.set(instance, []);
+  return eventsStore.get(instance)!;
+}
+
+/**
  * Base class for Aggregates that adds domain event management capabilities.
  * In Domain-Driven Design (DDD), only Aggregates (aggregate roots) should emit domain events.
  * Regular Entities and Value Objects should NOT manage domain events.
@@ -10,13 +23,12 @@ export abstract class BaseAggregate<
   T extends BaseProps,
   TOptionalInput extends keyof T = never,
 > extends BaseEntity<T, TOptionalInput> {
-  private domainEvents: IDomainEvent[] = [];
-
   /**
-   * Add a domain event to this aggregate
+   * Add a domain event to this aggregate.
+   * Safe to call inside the `onCreate` hook.
    */
   protected addDomainEvent(event: IDomainEvent): void {
-    this.domainEvents.push(event);
+    getEvents(this).push(event);
   }
 
   /**
@@ -31,34 +43,34 @@ export abstract class BaseAggregate<
    * Get all uncommitted domain events
    */
   getUncommittedEvents(): IDomainEvent[] {
-    return [...this.domainEvents];
+    return [...getEvents(this)];
   }
 
   /**
    * Clear all domain events (call after publishing)
    */
   clearEvents(): void {
-    this.domainEvents = [];
+    eventsStore.set(this, []);
   }
 
   /**
    * Check if aggregate has uncommitted events
    */
   hasUncommittedEvents(): boolean {
-    return this.domainEvents.length > 0;
+    return getEvents(this).length > 0;
   }
 
   /**
    * Get a domain event by name
    */
   getEvent(eventName: string): IDomainEvent | undefined {
-    return this.domainEvents.find((event) => event.eventName === eventName);
+    return getEvents(this).find((event) => event.eventName === eventName);
   }
 
   /**
    * Check if an event has been added to the aggregate
    */
   hasEvent(eventName: string): boolean {
-    return this.domainEvents.some((event) => event.eventName === eventName);
+    return getEvents(this).some((event) => event.eventName === eventName);
   }
 }
